6c82e8c30aa02f1ae63a39fd9645066b1978ce04
Leonardo Cox <leonardocx0@gmail.com>
2022-08-23 10:47:25 -0400

    Starting to mess with some groovy styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,65 @@
-import './App.css';
+import "./App.css";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import instacartLogo from "./logo.svg";
+import splash from "./splash.webp";
+import styled from "styled-components";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const Splash = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: url(${splash});
+`;
+
+const CoolContainer = styled(Container)`
+  color: red;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Navbar bg="light" expand="lg" fixed="top">
+        <Navbar.Brand style={{ margin: "5px 0 0 20px" }} href="#home">
+          <img src={instacartLogo} alt="Logo Image" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto"></Nav>
+          <Nav>
+            <Nav.Link href="#LogIn">Log In</Nav.Link>
+            <Button variant="success">Sign Up</Button>{" "}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Splash>
+        <Container>
+          <Row>
+            <Col>
+              <h1>Hello! Welcome to the Store!</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col>Some lovely goodies</Col>
+            <Col>Some other cool stuff</Col>
+            <Col>Ahh yes some more cool stuff</Col>
+          </Row>
+          <Row>
+            <Col>
+              <CoolContainer>
+                <Row>
+                  <Col>This should be cool</Col>
+                </Row>
+              </CoolContainer>
+            </Col>
+          </Row>
+        </Container>
+      </Splash>
+    </>
   );
 }
 
